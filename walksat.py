97e60815c6ff4fa6b6c6906541bc404a416261d0
Enric-Tobena <etc7@alumnes.udl.cat>
2023-03-18 20:38:32 +0100

    Algoritmo escogido a falta de simplificar

--- walksat.py
+++ walksat.py
@@ -21,74 +21,69 @@ def parse(filename):
     return clauses, n_vars, lit_clause
 
 
-def get_random_interpretation(n_vars):
+def random_inter(n_vars):
     return [i if random.randrange(2) == 0 else -i for i in range(n_vars + 1)]
-    #return {i: i if random.random() < 0.5 else -i for i in range(n_vars + 1)}
 
 
-def update_tsl(literal_to_flip, true_sat_lit, lit_clause):
+def true_literals(clauses, interpretation):
+    literals = [0] * len(clauses)
+    for i, clause in enumerate(clauses):
+        for literal in clause:
+            if interpretation[abs(literal)] == literal:
+                literals[i] += 1
+    return literals
+
+
+
+def update_literals(literal_to_flip, true_sat_lit, lit_clause):
     for clause_index in lit_clause[literal_to_flip]:
         true_sat_lit[clause_index] += 1
     for clause_index in lit_clause[-literal_to_flip]:
         true_sat_lit[clause_index] -= 1
 
 
-def compute_broken(clause, true_sat_lit, lit_in_clauses, interpretation, omega=1):
-    break_min = sys.maxsize
+
+def compute_break(clause, true_sat_lit, lit_in_clauses, omega=0.15):
     best_literals = []
+    break_min = sys.maxsize
     for literal in clause:
         break_score = 0
-        if interpretation[abs(literal)] < 0:
-            for clause_index in lit_in_clauses[-abs(literal)]:
-                if true_sat_lit[clause_index] == 1:
-                    break_score += 1
-                elif true_sat_lit[clause_index] == 0:
-                    break_score -= 1
-        else:
-            for clause_index in lit_in_clauses[abs(literal)]:
-                if true_sat_lit[clause_index] == 1:
-                    break_score += 1
-                elif true_sat_lit[clause_index] == 0:
-                    break_score -= 1
+        for clause_index in lit_in_clauses[-abs(literal)]:
+            if true_sat_lit[clause_index] == 1:
+                break_score += 1
         if break_score < break_min:
             break_min = break_score
             best_literals = [literal]
         elif break_score == break_min:
             best_literals.append(literal)
-    if break_min != 0 and random.random() < omega:
-        return random.sample(clause, 1)[0]
+    if random.random() < omega:
+        return random.choice(best_literals)
     else:
-        return random.sample(best_literals, 1)[0] if best_literals else None
+        return random.choice(clause)
 
 
-def get_true_sat_lit(clauses, interpretation):
-    true_sat_lit = [0] * len(clauses)
-    for clause_index, clause in enumerate(clauses):
-        for literal in clause:
-            if interpretation[abs(literal)] == literal:
-                true_sat_lit[clause_index] += 1
-    return true_sat_lit
 
 
-def run_sat(clauses, n_vars, lit_clause, max_flips_proportion=4):
-    max_flips = len(clauses) * max_flips_proportion * n_vars
-    interpretation = get_random_interpretation(n_vars)
-    true_sat_lit = get_true_sat_lit(clauses, interpretation)
-    for i in range(max_flips):
-        unsatisfied_clauses_index = [i for i, c in enumerate(clauses) if true_sat_lit[i] == 0]
-        if not unsatisfied_clauses_index:
+def sat(clauses, n_vars, lit_clause, max_flips_proportion=13):
+    max_flips = max_flips_proportion * n_vars * len(clauses)
+    interpretation = random_inter(n_vars)
+    true_lit = true_literals(clauses, interpretation)
+    for _ in range(max_flips):
+        unsatisfied_clauses = [i for i, x in enumerate(true_lit) if x == 0]
+        if not unsatisfied_clauses:
             return interpretation
-        clause_index = random.sample(unsatisfied_clauses_index, 1)[0]
-        unsatisfied_clause = clauses[clause_index]
-        lit_to_flip = compute_broken(unsatisfied_clause, true_sat_lit, lit_clause, interpretation)
-        if lit_to_flip is None:
+        clause_to_fix = random.choice(unsatisfied_clauses)
+        literal_to_flip = compute_break(clauses[clause_to_fix], true_lit, lit_clause)
+        if literal_to_flip is None:
             return None
-        update_tsl(lit_to_flip, true_sat_lit, lit_clause)
-        interpretation[abs(lit_to_flip)] = -interpretation[abs(lit_to_flip)]
+        interpretation[abs(literal_to_flip)] *= -1
+        update_literals(literal_to_flip, true_lit, lit_clause)
+
+    return None
 
 
 if __name__ == '__main__':
     clauses, n_vars, lit_clause = parse(sys.argv[1])
-    solution = run_sat(clauses, n_vars, lit_clause)
+    solution = sat(clauses, n_vars, lit_clause)
     print('s SATISFIABLE')
     print('v ' + ' '.join(map(str, solution[1:])) + ' 0')
